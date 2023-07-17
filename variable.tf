8c9076753c9cc3a68357065565385d04da34dcfb
Ben Chiro <bchiro333@gmail.com>
2023-07-17 21:01:44 +0100

    created vpc file

---
+++ variable.tf
@@ -0,0 +1,42 @@
+#vpc variable
+variable "vpc_cider" {
+    default     = "10.0.0.0/16"
+    description = "vpc cider block"
+    type        = string 
+}
+
+variable "public_subnet_az1_cider" {
+    default     = "10.0.0.0/24"
+    description = " public subnet az1 cider"
+    type        = string 
+}
+
+variable "public_subnet_az2_cider" {
+    default     = "10.0.1.0/24"
+    description = " public subnet az1 cider"
+    type        = string 
+}
+
+variable "private_app_subnet_az1_cider" {
+    default     = "10.0.2.0/24"
+    description = " private app subnet az1 cider"
+    type        = string 
+}
+
+variable "private_app_subnet_az2_cider" {
+    default     = "10.0.3.0/24"
+    description = " private app subnet az2 cider"
+    type        = string 
+}
+
+variable "private_data_subnet_az1_cider" {
+    default     = "10.0.4.0/24"
+    description = " private data subnet az1 cider"
+    type        = string 
+}
+
+variable "private_data_subnet_az2_cider" {
+    default     = "10.0.5.0/24"
+    description = " private data subnet az2 cider"
+    type        = string 
+}
